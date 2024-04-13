91182e542edd7ec858a2a30219d152ca6211ba82
heitzjulien <heitzjulien@pm.me>
2024-04-13 18:29:00 +0200

    feat: docker compose and sql script

---
+++ script.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `User`(
+    `user_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `firstname` VARCHAR(255) NOT NULL,
+    `lastname` VARCHAR(255) NOT NULL,
+    `profile_picture` TEXT NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL
+);
+CREATE TABLE `favorite_station`(
+    `station_code` VARCHAR(6) NOT NULL,
+    `user_id` BIGINT UNSIGNED NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `picture` TEXT NULL,
+    `name_custom` VARCHAR(255) NULL,
+    FOREIGN KEY(`user_id`) REFERENCES `User`(`user_id`) ON DELETE CASCADE,
+    PRIMARY KEY(`station_code`, `user_id`)
+);
