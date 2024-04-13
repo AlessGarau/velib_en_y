52500c58af100e5c2c7f5df27f597cbf70f12248
GDKobrae <djedjegboble.pro@gmail.com>
2024-04-13 20:39:20 +0200

    feat: Dockerfile for microsrvices and api-caching-server + update docker compose

---
+++ apps/microservices/favorite/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>KONO DIO DA !!!!!!</h1>"
+
+
+if __name__ == '__main__':
+    app.run()
