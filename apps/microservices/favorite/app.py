394b0042e52e47a704258c3c61522e1a09872f74
Louisan Tchitoula <t.louisan@outlook.fr>
2024-04-17 19:47:18 +0200

    feat(CU-86bya7wnn): pseudo code for crud operations

--- apps/microservices/favorite/app.py
+++ apps/microservices/favorite/app.py
@@ -1,11 +1,98 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, request, jsonify
+import mysql.connector
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "<h1>KONO DIO DA !!!!!!</h1>"
+db_config = {
+    'host': 'velyb-db',
+    'user': 'root',
+    'password': 'root',
+    'database': 'velyb'
+}
 
+def db_connection():
+    conn = None;
+    try:
+        conn = mysql.connector.connect(**db_config)
+    except BaseException as e:
+        assert(e)
+    return conn
+
+
+@app.route('/api/favorites', methods=['GET'])
+def user_favorites():
+    # Authorization with Auth Microservice
+    # If not authorized, return Error 401
+    # Else retrieve user_id
+    user_id = 1
+
+    try:
+        # DB Conntiton
+        conn = db_connection()
+
+        # Execute query to SELECT * FROM favorite_station WHERE user_id = user_id
+        cursor = conn.cursor(buffered=True)
+        cursor.execute("SELECT * FROM favorite_station WHERE user_id = %s", (user_id,))
+        user_favorites = cursor.fetchall()
+
+        # Return json with favorites items
+        return jsonify({'data': user_favorites}), 200
+    
+    except BaseException as e:
+        return jsonify({'message': str(e)}), 400
+
+
+
+@app.route('/api/favorites/<station_code>', methods=['DELETE'])
+def delete_favorite(station_code):
+    # Authorization with Auth Microservice
+    # If not authorized, return Error 401
+    # Else retrieve user_id
+
+    # First check if station code exists in favorites with SELECT * FROM favorite_station WHERE station_code = station_code AND user_id = user_id
+    # If doesn't exist, return Error 404
+
+    # try except 
+    # Execute query to DELETE FROM favorite_station WHERE station_code = station_code
+    # return 200
+    return jsonify({'foo': 'bar'}), 200
+
+@app.route('/api/favorites/<station_code>', methods=['PUT'])
+def update_favorite(station_code):
+    # Authorization with Auth Microservice
+    # If not authorized, return Error 401
+    # Else retrieve user_id
+
+    # First check if station code exists in favorites with SELECT * FROM favorite_station WHERE station_code = station_code AND user_id = user_id
+    # If doesn't exist, return Error 404
+
+    data = request.get_json()
+    # Validate inputs
+    # picture, name_custom
+
+    # try except 
+    # Execute query to UPDATE favorite_station SET ...values
+    # return 200
+    return jsonify({'foo': 'bar'}), 200
+
+@app.route('/api/favorites/', methods=['POST'])
+def create_favorite():
+    # Authorization with Auth Microservice
+    # If not authorized, return Error 401
+    # Else retrieve user_id
+
+    # First check if station code already exists in favorites with SELECT * FROM favorite_station WHERE station_code = station_code AND user_id = user_id
+    # If it does exist, return Error 400
+    
+
+    data = request.get_json()
+    # Validate inputs
+    # picture, name_custom, name, station_code
+
+    # try except 
+    # Execute query to INSERT INTO favorite_station ...values
+    # return 200
+    return jsonify({'foo': 'bar'}), 200
 
 if __name__ == '__main__':
     app.run()
