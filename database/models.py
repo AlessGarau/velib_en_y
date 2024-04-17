6f97b4eb0c5b41360edfdb3da3625c992a9b3290
Louisan Tchitoula <t.louisan@outlook.fr>
2024-04-17 20:25:12 +0200

    feat(models): add abstract and user model

--- database/models.py
+++
@@ -1,7 +0,0 @@
-class FavoriteStation:
-    def __init__(self, station_code, user_id, name, picture=None, name_custom=None) -> None:
-        self.station_code = station_code
-        self.user_id = user_id
-        self.name = name
-        self.picture = picture
-        self.name_custom = name_custom
