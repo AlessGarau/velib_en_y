16185b86d8d181eefbcea68e4d98868dfaf50d0d
Louisan Tchitoula <t.louisan@outlook.fr>
2024-04-18 04:13:42 +0200

    Add dbal example in favorite

--- apps/microservices/favorite/app.py
+++ apps/microservices/favorite/app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, jsonify
+from database_access_layer.database import connect_to_database
+from database_access_layer.models.favorite_station import FavoriteStation
 
 app = Flask(__name__)
+cnx = connect_to_database()
+
 
 @app.route('/')
 def index():
-    return "<h1>KONO DIO DA !!!!!!</h1>"
+    # Example
+    try:
+        favorite_stations = []
+        cursor = cnx.cursor(buffered=True)
+
+        query = "SELECT * FROM favorite_station"
+        cursor.execute(query)
+        for row in cursor.fetchall():
+            favorite_stations.append(FavoriteStation(*row).to_dict())
+        cursor.close()
+
+        return jsonify({'data': favorite_stations}), 200
+    except BaseException as e:
+        return jsonify({'message': str(e)}), 400
 
 
 if __name__ == '__main__':
