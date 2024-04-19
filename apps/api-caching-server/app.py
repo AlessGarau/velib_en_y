8a98dab79086512a89cbcbe9f7bcfb184228774b
Alessandro Garau <alessandrogarau.pro@gmail.com>
2024-04-19 18:25:28 +0200

    fix: la route avec le timer

--- apps/api-caching-server/app.py
+++ apps/api-caching-server/app.py
@@ -1,35 +1,40 @@
 from flask import Flask, jsonify
 import socket
 import requests
+import time
 
 app = Flask(__name__)
 
+last_api_call = 0
+api_data = requests.get(
+    'https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records?limit=100')
 
-@app.route('/')
+
+def check_api_call():
+    global last_api_call
+    current_time = time.time()
+    if current_time - last_api_call > 300:
+        last_api_call = current_time
+        return True
+    else:
+        return False
+
+
+def call_api(api_data):
+    if check_api_call():
+        response = requests.get(
+            'https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records?limit=100')
+        api_data = response.json()
+        return api_data
+    return api_data.json()
+
+
+@ app.route('/')
 def index():
     return '<h1>Streamez <a href="https://open.spotify.com/track/6TzeXZyF3ULjwFz64eoXUc?si=a944a36fff674166">BU$HI </a></h1>'
 
 
-@app.route('/get_all_velib', methods=['GET'])
+@ app.route('/get_all_velib', methods=['GET'])
 def get_data():
-    response = requests.get(
-        'https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records?limit=100')
-    data = response.json()
-
-    return jsonify(data)
-
-
-@app.route('/guest', methods=['GET'])
-def get_data_guest():
-    response = requests.get(
-        'https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records?limit=100')
-    data = response.json()
-    return data
-
-
-@app.route('/get_favorite',  methods=['GET'])
-def get_favorite():
-    response = requests.get(
-        'https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records?where=stationcode%3D%2219029%22&limit=100')
-    data = response.json()
-    return data
+    all_velib = call_api(api_data)
+    return jsonify(all_velib)
