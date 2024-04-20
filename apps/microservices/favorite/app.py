377d6e516756a3d2f28ffb7737f97d5a9fbc69d4
Louisan Tchitoula <t.louisan@outlook.fr>
2024-04-21 00:10:48 +0200

    CU-86bya7wnn Fix non existant user when creating favorite

--- apps/microservices/favorite/app.py
+++ apps/microservices/favorite/app.py
@@ -8,10 +8,22 @@ app = Flask(__name__)
 cnx = connect_to_database()
 
 
-def favorite_station_exists(user_id: str, station_code: str, cursor: MySQLCursor) -> bool:
+def favorite_station_exists(user_id: str, station_code: str, cursor: MySQLCursor, check_user: bool = False) -> bool:
     """
     Checks that the favorite station targeted (user_id and station_code) exists in the db
     """
+
+    if check_user:
+        select_query = """
+            SELECT * 
+            FROM user
+            WHERE user_id=%s
+        """
+        cursor.execute(select_query, (user_id,))
+        user = cursor.fetchone()
+
+        if not (user or len(user) > 0):
+            raise BaseException("User does not exist.")
 
     select_query = """
         SELECT * 
@@ -19,9 +31,9 @@ def favorite_station_exists(user_id: str, station_code: str, cursor: MySQLCursor
         WHERE user_id=%s AND station_code=%s
     """
     cursor.execute(select_query, (user_id, station_code,))
-    row = cursor.fetchall()
+    favorite_stations = cursor.fetchall()
 
-    return len(row) > 0
+    return len(favorite_stations) > 0
 
 
 @app.route('/api/favorites', methods=['GET'])
@@ -141,11 +153,11 @@ def create_favorite():
     try:
         cursor = cnx.cursor(buffered=True)
 
-        if favorite_station_exists(user_id, station_code, cursor):
-            raise BaseException(f"Favorite station {station_code} already exists")
-
         if not all((station_code, user_id, name, picture, name_custom,)):
             raise BaseException("Credentials are not valid.")
+
+        if favorite_station_exists(user_id, station_code, cursor, True):
+            raise BaseException(f"Favorite {station_code} already exists")
 
         insert_query = """INSERT INTO favorite_station VALUES(%s, %s, %s, %s, %s)"""
         cursor.execute(insert_query, (station_code, user_id, name, picture, name_custom,))
