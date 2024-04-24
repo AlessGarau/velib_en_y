651aed285ceafc94250e5309b64e6753d386319c
Louisan Tchitoula <t.louisan@outlook.fr>
2024-04-24 15:10:22 +0200

    Remove unused script and classes & Add custom protocol

--- apps/api-caching-server/app.py
+++ apps/api-caching-server/app.py
@@ -1,57 +1,31 @@
-import socket
 import requests
-import time
+import os
+
+from cache_protocol_models.cache_protocol import CacheProtocol
+
+ADDRESS_HOST = os.getenv('FLASK_RUN_HOST')
+PORT_DEFAULT = os.getenv('FLASK_RUN_PORT')
 
 
-api_url = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records?limit=100"
-last_api_call = 0
+class OpenDataCache(CacheProtocol):
+    VELIB_COUNT = 0
 
-data_cached = requests.get(api_url).json()
-velib_count = data_cached.get("total_count")
+    def __init__(self, ADDRESS_HOST, PORT_DEFAULT, endpoint_to_cache: str, delay: int) -> None:
+        super().__init__(ADDRESS_HOST, PORT_DEFAULT, endpoint_to_cache, delay)
 
+    def set_cache(self) -> None:
+        res = requests.get(self.API_URL).json()
+        self.VELIB_COUNT = res.get('total_count')
 
-def check_api_call():
-    global last_api_call
-    current_time = time.time()
-    if current_time - last_api_call > 300:
-        last_api_call = current_time
-        return True
-    else:
-        return False
+        for offset in range(0, self.VELIB_COUNT, 100):
+            next_res = requests.get(self.API_URL + f"offset={offset}").json()
+            res["results"].append(next_res)
 
-
-def call_api():
-    global data_cached
-    global velib_count
-    if check_api_call():
-        data = requests.get(api_url).json()
-        velib_count = data.get("total_count")
-        for offset in range(0, velib_count, 100):
-            response = requests.get(api_url + f"&offset={offset}")
-            new_data = response.json()
-            data["results"].append(new_data)
-        data_cached = data
-    return data_cached
+        self.CACHE = res
 
 
-HOST = 'localhost'
-PORT = 8004
-
-
-def tcp_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen(1)
-    print(f"Server listening on {HOST}:{PORT}")
-
-    while True:
-        client_socket, address = server_socket.accept()
-        print(f"Connection received from {address}")
-
-        data = call_api()
-        client_socket.sendall(str(data).encode())
-
-        client_socket.close()
-
-
-tcp_server()
+if __name__ == "__main__":
+    server = OpenDataCache(ADDRESS_HOST,
+                           PORT_DEFAULT,
+                           "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records?limit=100",
+                           300)
