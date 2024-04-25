d4aa387ea4584dc1fd4f5e4bd9ca6d0a21da1595
Louisan Tchitoula <t.louisan@outlook.fr>
2024-04-25 19:10:41 +0200

    Add custom css for marker clusters

--- apps/velyb-web-server/ressources/js/map.js
+++ apps/velyb-web-server/ressources/js/map.js
@@ -1,34 +1,35 @@
 // TODO - Essayer de garder dernière position en localStorage
-// TODO - Si l'user est connecté, remplir la liste favorite_stations 
+// TODO - Si l'user est connecté, remplir la liste favorite_stations
 
 // Set initial view
-const map = L.map('map').setView([48.8566, 2.3522], 13);
+const map = L.map("map").setView([48.8566, 2.3522], 13);
 const mcg = L.markerClusterGroup();
-L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    className: 'map-tiles'
+L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  className: "map-tiles",
 }).addTo(map);
 
 class VelybMap {
-    stations_per_commune = new Object();
-    favorite_stations = new Array();
-    total_count = 0;
+  stations_per_commune = new Object();
+  favorite_stations = new Array();
+  total_count = 0;
 
-    constructor(data_url, selected_neighborhood, user_id = null) {
-        this.data_url = data_url;
-        this.selected_neighborhood = selected_neighborhood;
-        this.user_id = user_id;
-    }
+  constructor(data_url, selected_neighborhood, user_id = null) {
+    this.data_url = data_url;
+    this.selected_neighborhood = selected_neighborhood;
+    this.user_id = user_id;
+  }
 
-    async set_stations() {
-        await axios.get(this.data_url)
-            .then(res => {
-                this.total_count = res.data.total_count;
+  async set_stations() {
+    await axios
+      .get(this.data_url)
+      .then((res) => {
+        this.total_count = res.data.total_count;
 
-                const markers = res.data.results.map(station => {
-                    const coordinates = [station.coordonnees_geo.lat, station.coordonnees_geo.lon];
-                    const marker = L.marker(coordinates);
-                    marker.bindPopup(`
+        const markers = res.data.results.map((station) => {
+          const coordinates = [station.coordonnees_geo.lat, station.coordonnees_geo.lon];
+          const marker = L.marker(coordinates);
+          marker.bindPopup(`
                                 <b>${station.name}</b>
                                 <br/>
                                 Nombre de vélos disponibles : ${station.numbikesavailable}
@@ -36,24 +37,23 @@ class VelybMap {
                                 Type : Mécanique : ${station.mechanical}/ Électrique : ${station.ebike}
                                 <br/>
                                 Nombre de places libres : ${station.numdocksavailable}
-                            `)
+                            `);
 
-                    if (!this.stations_per_commune.hasOwnProperty(station.nom_arrondissement_communes)) {
-                        this.stations_per_commune[station.nom_arrondissement_communes] = new Array();
-                    }
-                    this.stations_per_commune[station.nom_arrondissement_communes].push({'station': station, 'marker': marker});
+          if (!this.stations_per_commune.hasOwnProperty(station.nom_arrondissement_communes)) {
+            this.stations_per_commune[station.nom_arrondissement_communes] = new Array();
+          }
+          this.stations_per_commune[station.nom_arrondissement_communes].push({ station: station, marker: marker });
 
-                    mcg.addLayer(marker);
-                    return marker;
-                });
+          mcg.addLayer(marker);
+          return marker;
+        });
 
-                mcg.addTo(map);
-                // debugger;
-            })
-            .catch(error => console.error(error));
-    }
-
+        mcg.addTo(map);
+        // debugger;
+      })
+      .catch((error) => console.error(error));
+  }
 }
 
-const velybMap = new VelybMap('http://localhost:8004/', 'Paris')
+const velybMap = new VelybMap("http://localhost:8004/", "Paris");
 velybMap.set_stations();
