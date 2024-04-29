a59551a2528a1375a66d73c5ed2e40238b3190fb
Louisan Tchitoula <t.louisan@outlook.fr>
2024-04-29 19:35:35 +0200

    Fix host in endpoints

--- apps/velyb-web-server/loaders/station_loaders.py
+++ apps/velyb-web-server/loaders/station_loaders.py
@@ -1,8 +1,11 @@
+import os
 from flask import jsonify
 import requests
 
+cache_host = os.getenv('CACHE_HOST')
+
 
 def get_stations():
-    res = requests.get('http://api-caching-server:8004/')
+    res = requests.get(f'http://{cache_host}:8004/')
 
     return jsonify(res.json()), res.status_code
